11a620cf5ffc00eab252a3daa432988431077890
iFengJQ <ifengjq@qq.com>
2020-06-11 10:37:35 +0800

    style: 修改 #19 的注释风格

--- 19.删除链表的倒数第n个节点.java
+++ 19.删除链表的倒数第n个节点.java
@@ -6,8 +6,12 @@
 
 // @lc code=start
 /**
- * Definition for singly-linked list. public class ListNode { int val; ListNode
- * next; ListNode(int x) { val = x; } }
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
